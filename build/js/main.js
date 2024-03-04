59025548c5b42fa6f3fb957ba8919fc67ecc5920
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2024-03-04 13:29:18 +0100

    problem with table row is resolved

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,4 @@
 "use strict";
-// let p:number;
-// let r:number;
-// let n:number;
-// type inputValues = number
 //convert year to month
 let yearToMonth = (n) => {
     return n * 12;
@@ -23,8 +19,52 @@ const userMortgageSize = document.querySelector('#mortgageSize');
 const interestRate = document.querySelector('#interestRate');
 //get the mortgage duration
 const payBackPlan = document.querySelector('#payBackPlan');
+//get the tabel
+const resultsTable = document.querySelector('.resultsTabel');
+//get the wrong input div
+let wrongCard = document.querySelector('.wrongInputResult');
+//get the wrong input message div
+let wrongMessage = document.querySelector('.wrongInput');
+// class for implementing the interface och getting the table row
+class TableResults {
+    constructor(mortgage, rate, numberOfYears, monthPayment) {
+        this.mortgage = mortgage;
+        this.rate = rate;
+        this.numberOfYears = numberOfYears;
+        this.monthPayment = monthPayment;
+    }
+    result() {
+        let test = `<tr>
+        <td>${this.mortgage}</th>
+        <td>${this.rate}</th>
+        <td>${this.numberOfYears}</th>
+        <td>${this.monthPayment}</th>
+    </tr>`;
+        return test;
+    }
+}
 //event handling
 calculateBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(monthlyPayment(userMortgageSize.valueAsNumber, interestRate.valueAsNumber, payBackPlan.valueAsNumber).toFixed());
+    //check if the mortgage size is for large
+    if (userMortgageSize.valueAsNumber > 100000000) {
+        wrongCard.style.display = 'flex';
+        wrongMessage.innerHTML = 'Jag tror inte banken har så mycket pengar!';
+    }
+    //check if the rate is over the edge
+    else if (interestRate.valueAsNumber > 10) {
+        wrongCard.style.display = 'flex';
+        wrongMessage.innerHTML = 'Jag tror inte vi behöver så mycket pengar!';
+    }
+    //check if the pay back plan is longer than usual
+    else if (payBackPlan.valueAsNumber > 35) {
+        wrongCard.style.display = 'flex';
+        wrongMessage.innerHTML = 'Hoppas du hinner betala tillbacka så mycket pengar!';
+    }
+    //add the table row if everything is in check
+    else {
+        const result = new TableResults(userMortgageSize.valueAsNumber, interestRate.valueAsNumber, payBackPlan.valueAsNumber, monthlyPayment(userMortgageSize.valueAsNumber, interestRate.valueAsNumber, payBackPlan.valueAsNumber));
+        // add the row to the previous
+        resultsTable.innerHTML += result.result();
+    }
 });
--- scr/main.ts
+++ scr/main.ts
@@ -1,8 +1,4 @@
-// let p:number;
-// let r:number;
-// let n:number;
 
-// type inputValues = number
 //convert year to month
 let yearToMonth = (n:number): number => {
     return n * 12
@@ -22,17 +18,73 @@ const monthlyPayment = (p:number, r:number, n:number): number =>{
 
 
 //get the calculate button
-const calculateBtn = document.querySelector('#calculateBtn') as HTMLButtonElement; 
+const calculateBtn = document.querySelector('#calculateBtn') as HTMLButtonElement
 
 //get the mortgage size
-const userMortgageSize = document.querySelector('#mortgageSize') as HTMLInputElement;
+const userMortgageSize = document.querySelector('#mortgageSize') as HTMLInputElement
 //get the mortgage rate
-const interestRate = document.querySelector('#interestRate') as HTMLInputElement;
+const interestRate = document.querySelector('#interestRate') as HTMLInputElement
 //get the mortgage duration
-const payBackPlan = document.querySelector('#payBackPlan') as HTMLInputElement;
+const payBackPlan = document.querySelector('#payBackPlan') as HTMLInputElement
+//get the tabel
+const resultsTable = document.querySelector('.resultsTabel') as HTMLTableElement
+//get the wrong input div
+let wrongCard = document.querySelector('.wrongInputResult') as HTMLDivElement
+//get the wrong input message div
+let wrongMessage = document.querySelector('.wrongInput') as HTMLDivElement
+
+// interface for calculating the results
+interface CalculationResults{
+    mortgage: number,
+    rate:number,
+    numberOfYears:number,
+    monthPayment:number
+}
+
+
+// class for implementing the interface och getting the table row
+class TableResults implements CalculationResults{
+    constructor(public mortgage:number, public rate:number, public numberOfYears:number, public monthPayment:number) {}
+    result(){ 
+        let test:string = `<tr>
+        <td>${this.mortgage}</th>
+        <td>${this.rate}</th>
+        <td>${this.numberOfYears}</th>
+        <td>${this.monthPayment}</th>
+    </tr>`
+
+        return test   
+    }
+}
+
 
 //event handling
 calculateBtn.addEventListener('click', (e: Event) => {
     e.preventDefault()
-    console.log(monthlyPayment(userMortgageSize.valueAsNumber, interestRate.valueAsNumber, payBackPlan.valueAsNumber).toFixed())
+    //check if the mortgage size is for large
+    if (userMortgageSize.valueAsNumber > 100000000){
+        wrongCard.style.display = 'flex'
+        wrongMessage.innerHTML = 'Jag tror inte banken har så mycket pengar!'
+    }
+    //check if the rate is over the edge
+    else if(interestRate.valueAsNumber > 10) {
+        wrongCard.style.display = 'flex'
+        wrongMessage.innerHTML = 'Jag tror inte vi behöver så mycket pengar!'
+    }
+    //check if the pay back plan is longer than usual
+    else if(payBackPlan.valueAsNumber > 35) {
+        wrongCard.style.display = 'flex'
+        wrongMessage.innerHTML = 'Hoppas du hinner betala tillbacka så mycket pengar!'
+    }
+    //add the table row if everything is in check
+    else { const result = new TableResults(userMortgageSize.valueAsNumber, interestRate.valueAsNumber, payBackPlan.valueAsNumber,  monthlyPayment(userMortgageSize.valueAsNumber, interestRate.valueAsNumber, payBackPlan.valueAsNumber))
+
+        // add the row to the previous
+        resultsTable.innerHTML += result.result()
+    }
+    
+    
+    
+ 
+    
 })
